53e2e9a7651f24be1beaf8a83de3c4686cebeda7
unknown <damasio34@gmail.com>
2015-09-22 15:03:41 -0300

    Implementação de novas funcionalidades

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,7 @@
 (function(angular) {
 
-    var controllers = angular.module('wopoTodolist.controllers', [])	
-	controllers.controller('TodoListController', function($scope, $state, TodoService, LoginService) {
-		
-		TodoService.getAll().success(function(data){
-			$scope.items = data.results;
-		});
-	
-		$scope.onItemDelete = function(item){
-			TodoService.delete(item.objectId);
-			$scope.items.splice($scope.items.indexOf(item), 1);
-		};
-		
-		$scope.signOut = function() { 
-		LoginService.logout($scope.Model).success(function() {
-				$state.go('login');
-			});
-		};
-		
-	});
-	
+    var controllers = angular.module('wopoTodolist.controllers', []);
+
 	controllers.controller('LoginController', function($scope, $state, LoginService) {
 		
 		if (LoginService.usuarioAutenticado()) $state.go('todos');
@@ -73,37 +55,61 @@
 		};
 		
 	});
+	
+	controllers.controller('TodoListController', function($scope, $state, TodoService, LoginService) {
+			
+		var self = this;
+		this.getItens = function(callack) {		
+			TodoService.getAll().success(function(data) {
+				$scope.items = data.results;
+				if (callack) callack();
+			});
+		};
+		
+		this.getItens();		
+				
+		$scope.doRefresh = function() {
+			self.getItens(function() {
+				//Stop the ion-refresher from spinning
+				$scope.$broadcast('scroll.refreshComplete');
+			});
+		};		
+	
+		$scope.onItemDelete = function(item) {
+			TodoService.excluir(item.objectId);
+			$scope.items.splice($scope.items.indexOf(item), 1);
+		};
+		
+		$scope.signOut = function() { 
+		LoginService.logout($scope.Model).success(function() {
+				$state.go('login');
+			});
+		};
+		
+	});
+	
+	controllers.controller('TodoCreationController', function($scope, TodoService, $state) {
+
+		$scope.model = {};
+	
+		$scope.incluir = function(){
+			TodoService.incluir($scope.model).success(function(data) {
+				$state.go('todos');
+			});
+		}
+
+	});
+
+	controllers.controller('TodoEditController', function($scope, TodoService, $state, $stateParams) {
+
+		$scope.model = { objectId: $stateParams.id, content: $stateParams.content };
+	
+		$scope.editar = function() {
+			TodoService.editar($scope.model).success(function(data) {
+				$state.go('todos');
+			});
+    	}
+
+	});	
         
 })(angular);
-// angular.module('wopoTodolist.controllers', [])
-// 	.controller('TodoListController', ['$scope', 'Todo', function($scope, Todo){
-
-//     Todo.getAll().success(function(data){
-//         $scope.items = data.results;
-//     });
-
-//     $scope.onItemDelete = function(item){
-//         Todo.delete(item.objectId);
-//         $scope.items.splice($scope.items.indexOf(item), 1);
-//     }
-
-// }]).controller('TodoCreationController', ['$scope', 'Todo', '$state', function($scope, Todo, $state){
-
-//     $scope.todo = {};
-
-//     $scope.create = function(){
-//         Todo.create({ content:$scope.todo.content }).success(function(data){
-//             $state.go('todos');
-//         });
-//     }
-// }]).controller('TodoEditController', ['$scope', 'Todo', '$state', '$stateParams', function($scope, Todo, $state, $stateParams){
-
-//     $scope.todo = { id:$stateParams.id, content:$stateParams.content };
-
-//     $scope.edit = function(){
-//         Todo.edit($scope.todo.id,{content:$scope.todo.content}).success(function(data){
-//             $state.go('todos');
-//         });
-//     }
-
-// }]);
